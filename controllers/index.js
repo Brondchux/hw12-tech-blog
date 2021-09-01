a2ec9147210fc33dfec7b746abe46ff74e0598cb
Gospel Chukwu <brondchux@gmail.com>
2021-08-31 20:30:28 -0400

    Added signup interface, introduced fontawesome. looks good

--- controllers/index.js
+++ controllers/index.js
@@ -2,9 +2,11 @@ const router = require("express").Router();
 const apiRoutes = require("./api");
 const homeRoutes = require("./home-routes");
 const loginRoutes = require("./login-routes");
+const signupRoutes = require("./signup-routes");
 
 router.use("/", homeRoutes);
 router.use("/api", apiRoutes);
 router.use("/login", loginRoutes);
+router.use("/signup", signupRoutes);
 
 module.exports = router;
