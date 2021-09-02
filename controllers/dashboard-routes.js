31361a01d889b35ed8d54786b10b573b2d917d6e
Gospel Chukwu <brondchux@gmail.com>
2021-09-01 21:38:03 -0400

    Introduced handlebars helpers

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -3,7 +3,7 @@ const router = require("express").Router();
 router.get("/", (req, res) => {
 	res.render("dashboard", {
 		userData: req.session.user,
-		timer: "June 21, 2021 17:00:00",
+		timer: new Date(),
 	});
 });
 
--- utils/helpers.js
+++ utils/helpers.js
@@ -1,13 +1,5 @@
 module.exports = {
 	dateFormatter: (date) => {
-		return date.toLowerCase();
+		return date.toLocaleDateString();
 	},
 };
-
-// module.exports = {
-// 	// the helper method 'format_time' will take in a timestamp and return a string with only the time
-// 	format_time: (date) => {
-// 		// We use the 'toLocaleTimeString()' method to format the time as H:MM:SS AM/PM
-// 		return date.toLocaleTimeString();
-// 	},
-// };
