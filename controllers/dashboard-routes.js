138b7c3f48952a9f2b05e308fc9301f11c4ea8f7
Gospel Chukwu <brondchux@gmail.com>
2021-09-03 13:11:31 -0400

    Included a conditional display of edit and delete button on each post

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,9 +1,26 @@
 const router = require("express").Router();
+const { Post, User } = require("../models");
 
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
+	let userPosts;
+	try {
+		const userPostsData = await Post.findAll({
+			where: {
+				user_id: req.session.user.id,
+			},
+			include: [{ model: User, attributes: ["username"] }],
+		});
+		userPosts = userPostsData.map((post) => {
+			const plainPost = post.get({ plain: true });
+			return { ...plainPost, postOwner: true };
+		});
+	} catch (error) {
+		userPosts = null;
+	}
 	res.render("dashboard", {
 		userData: req.session.user,
 		loggedIn: req.session.loggedIn,
+		userPosts,
 	});
 });
 
